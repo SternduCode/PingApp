54ed8859c25bad7dcc2157f716cfb5c9c55098af
Sterndu <Sterndu13570@gmail.com>
2023-01-02 07:25:32 +0100

    Fixed some warnings

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.sterndu.pingonvolcano.android"
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.sterndu.pingonvolcano.android"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
@@ -33,21 +33,21 @@ android {
 
 dependencies {
 
-    val nav_version = "2.5.3"
+    val navVersion = "2.5.3"
 
     implementation(project(":shared"))
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
-    implementation("androidx.navigation:navigation-compose:$nav_version")
-    implementation("androidx.emoji:emoji:1.0.0")
+    implementation("androidx.compose.ui:ui:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
+    implementation("androidx.compose.foundation:foundation:1.3.1")
+    implementation("androidx.compose.material:material:1.3.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+    implementation("androidx.emoji:emoji:1.1.0")
     implementation("com.google.firebase:protolite-well-known-types:18.0.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.4.+")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -70,9 +70,9 @@ kotlin {
 
 android {
     namespace = "com.sterndu.pingonvolcano"
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
     }
 }
